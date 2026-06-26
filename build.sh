02c94ad84789057ea50f3f1a5594a06af5742232
Atul Gangwar <atulgangwar536@gmail.com>
2026-06-26 21:24:59 +0530

    Auto-create founder superuser in build.sh for Render free tier

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='founder').exists() or User.objects.create_superuser('founder', 'founder@example.com', 'Founder@123')"
