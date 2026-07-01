0aba11087538ef705e4f8a3826f492a8e429cb71
Atul Gangwar <atulgangwar536@gmail.com>
2026-07-01 22:55:31 +0530

    Temporarily enable DEBUG to view 500 error traceback

--- dsa_platform/settings.py
+++ dsa_platform/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&-=z+i01y6_vvg*a9lqc!9=cucq_-2j5vb+vn^#)z@6lo$8%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
